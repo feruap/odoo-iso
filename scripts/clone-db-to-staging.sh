e3b0c671ec1dd054221e36c6c10e96a07bfbd3f2
DavidG1133 <gonzalezdavid11.3.3@hotmail.com>
2026-04-07 14:39:41 -0600

    fix: corregir PROD_DB a odoo_production confirmado por filestore de produccion

--- scripts/clone-db-to-staging.sh
+++ scripts/clone-db-to-staging.sh
@@ -3,7 +3,7 @@
 set -e
 PROD_CONTAINER="odoo-production-db"
 STAGING_CONTAINER="odoo-staging-db"
-PROD_DB="amunet_prod"
+PROD_DB="odoo_production"
 STAGING_DB="Amunet_testing"
 PROD_DATA_VOL="production_odoo-production-data"
 STAGING_DATA_VOL="staging_odoo-staging-data"
@@ -26,7 +26,7 @@ docker cp "$BACKUP_FILE" "${STAGING_CONTAINER}:${CONTAINER_BACKUP}"
 docker exec "$STAGING_CONTAINER" pg_restore -U odoo -d "$STAGING_DB" --no-owner --no-privileges "$CONTAINER_BACKUP" || true
 docker exec "$STAGING_CONTAINER" rm -f "$CONTAINER_BACKUP"
 echo "[4/5] Copiando filestore..."
-docker run --rm -v "${PROD_DATA_VOL}:/prod:ro" -v "${STAGING_DATA_VOL}:/staging" alpine sh -c "rm -rf /staging/filestore && cp -r /prod/filestore /staging/filestore && mv /staging/filestore/amunet_prod /staging/filestore/Amunet_testing"
+docker run --rm -v "${PROD_DATA_VOL}:/prod:ro" -v "${STAGING_DATA_VOL}:/staging" alpine sh -c "rm -rf /staging/filestore && cp -r /prod/filestore /staging/filestore && mv /staging/filestore/odoo_production /staging/filestore/Amunet_testing"
 echo "[5/5] Reiniciando Odoo staging..."
 docker start odoo-staging
 rm -f "$BACKUP_FILE"
