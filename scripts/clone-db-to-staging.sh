7f3fbc09fcf4a2989a78dcdd489677772b798f36
DavidG1133 <gonzalezdavid11.3.3@hotmail.com>
2026-04-07 14:32:41 -0600

    chore: quitar verbose de pg_restore para output limpio

--- scripts/clone-db-to-staging.sh
+++ scripts/clone-db-to-staging.sh
@@ -23,7 +23,7 @@ docker exec "$STAGING_CONTAINER" psql -U odoo -c "DROP DATABASE IF EXISTS \"$STA
 docker exec "$STAGING_CONTAINER" psql -U odoo -c "CREATE DATABASE \"$STAGING_DB\";" postgres
 # Copiar dump al contenedor y restaurar con pg_restore (no tiene restricciones de backslash)
 docker cp "$BACKUP_FILE" "${STAGING_CONTAINER}:${CONTAINER_BACKUP}"
-docker exec "$STAGING_CONTAINER" pg_restore -U odoo -d "$STAGING_DB" --no-owner --no-privileges -v "$CONTAINER_BACKUP" || true
+docker exec "$STAGING_CONTAINER" pg_restore -U odoo -d "$STAGING_DB" --no-owner --no-privileges "$CONTAINER_BACKUP" || true
 docker exec "$STAGING_CONTAINER" rm -f "$CONTAINER_BACKUP"
 echo "[4/5] Copiando filestore..."
 docker run --rm -v "${PROD_DATA_VOL}:/prod:ro" -v "${STAGING_DATA_VOL}:/staging" alpine sh -c "rm -rf /staging/filestore && cp -r /prod/filestore /staging/filestore && mv /staging/filestore/amunet_prod /staging/filestore/Amunet_testing"
