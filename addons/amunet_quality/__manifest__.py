d952610e5e4ba80356480deda42b42e2362c951b
Agente Amunet <agente@amunet.com.mx>
2026-05-19 00:21:48 +0200

    feat quality add self service signature PIN menu

--- addons/amunet_quality/__manifest__.py
+++ addons/amunet_quality/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Amunet - Control de Calidad',
-    'version': '19.0.3.3.0',
+    'version': '19.0.3.4.0',
     'category': 'Quality',
     'summary': 'Sistema de Control de Calidad con Parámetros Jerárquicos',
     'description': """
