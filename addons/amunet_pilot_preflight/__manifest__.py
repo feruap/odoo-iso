b60dcf10abf121f84b1d87b8bc21e84ca7cf4591
Agente Amunet <agente@amunet.com.mx>
2026-05-20 17:09:58 +0200

    Add pilot preflight checklist

---
+++ addons/amunet_pilot_preflight/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Amunet - Preflight Piloto',
+    'summary': 'Checklist preventivo antes de fabricar un piloto',
+    'description': """
+        Valida antes de iniciar un piloto de fabricacion: BOM, stock,
+        calidad, muestreo, empaque, usuarios, capacitacion, equipos y
+        cambios abiertos. Es una capa preventiva de ISO 13485; no sustituye
+        firmas ni liberaciones formales.
+    """,
+    'author': 'Amunet',
+    'category': 'Manufacturing',
+    'version': '19.0.1.0.0',
+    'depends': [
+        'mail',
+        'mrp',
+        'stock',
+        'amunet_production',
+        'amunet_quality',
+        'amunet_packaging_planning',
+        'amunet_material_request',
+        'amunet_competencias',
+        'amunet_equipment_calibration',
+        'amunet_change_control',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ir_sequence_data.xml',
+        'views/pilot_preflight_views.xml',
+        'views/mrp_production_views.xml',
+        'views/menu_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ addons/amunet_pilot_preflight/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import pilot_preflight
+from . import mrp_production
